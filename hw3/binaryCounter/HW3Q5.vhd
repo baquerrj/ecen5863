a6690901b4d19477d37145a4edf3fee617d2898b
Roberto Baquerizo <roba8460@colorado.edu>
2019-09-30 19:50:38 -0600

    4-bit binary counter vhdl implementation

--- hw3/binaryCounter/HW3Q5.vhd
+++ hw3/binaryCounter/HW3Q5.vhd
@@ -4,8 +4,8 @@
 --                                                                            --
 --------------------------------------------------------------------------------
 --
--- @file HW3P4.vhd
--- @brief Application Assignment 2-001 Example code with errors to be found
+-- @file HW3Q5.vhd
+-- @brief a VHDL model of the 74LS163 4-bit binary counter
 -- @version: 1.0
 -- Date of current revision:  @date 2019-09-29
 -- Target FPGA: [Intel Altera MAX10]
@@ -14,10 +14,11 @@
 --             [Quartus Prime 16.1]  for place and route if applied
 --
 --  Functional Description:  This file contains the VHDL which describes the
---               FPGA implementation of a Majority Vote Circuit. The inputs are
---               signals A, B, C with one output Y that is true only if at
---               least 2 of the inputs are true.
-
+--               FPGA implementation of the 74LS163 4-bit binary counter. The
+--               74LS163 counts modulo 16. It has a Synchronous Reset (Clear) input
+--               that overrides all other control inputs, but is active only
+--               during the rising clock edge.
+--
 --  Hierarchy:  There is only one level in this simple design.
 --
 --  Designed by:  @author Roberto Baquerizo
@@ -36,15 +37,49 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-entity HW3Q5 is port (                 	
+entity HW3Q5 is port (
    CP: 	in std_logic; 	-- clock
    SR:	in std_logic;  -- Active low, synchronous reset
-   P:    in std_logic_vector[3 downto 0];  -- Parallel input
+   P:    in std_logic_vector(3 downto 0);  -- Parallel input
    PE:   in std_logic;  -- Parallel Enable (Load)
-   CEP: in std_logic;  -- Count enable parallel input
+   CEP:  in std_logic;  -- Count enable parallel input
    CET:  in std_logic; -- Count enable trickle input
-   Q:   out std_logic_vector[3 downto 0];            			
-   TC:  out std_logic  -- Terminal Count
-);            		
-end HW3Q5;                   	          	
+   Q:    out std_logic_vector(3 downto 0);
+   TC:   out std_logic  -- Terminal Count
+);
+end entity HW3Q5;
+
+architecture HW5Q5_arch of HW3Q5 is
+   -- CE (Count Enable) is true when (CEP and CET and PE)
+   signal CE : std_logic := '0';
+	-- interal signal to hold value of Q
+	signal counter : unsigned(3 downto 0) := "0000";
+
+begin
+	-- concurrent statments
+	Q <= std_logic_vector(counter);
+	CE <= '1' when ((CEP='1') and (CET='1') and
+						(PE='1')) else '0';
+   TC <= '1' when ((CET='1') and (counter="1111")) else '0';
+
+	-- main process checks input signals on rising clock edge
+	main : process(CP)
+   begin
+      if rising_edge(CP) then
+         if SR = '0' then
+				-- synchronous reset
+            counter <= "0000";
+         elsif PE = '0' then
+				-- parallel load inputs to counter value
+            for i in counter'range loop
+               counter(i) <= P(i);
+            end loop;
+         elsif CE = '1' then
+				-- normal operation (increment counter)
+				counter <= counter + 1;
+			end if;
+      end if;
+	end process main;
+end architecture HW5Q5_arch;
