9cdb0f846add0738c6aca5a36d1924e9083b4307
Roberto Baquerizo <roba8460@colorado.edu>
2019-10-03 20:35:20 -0600

    binary counter verilog model for problem 2

---
+++ hw4/p2/HW4P2.v
@@ -0,0 +1,79 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//               Application Assignment 4 Module 1 Course 2                   //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+//
+// @file comparator.v
+// @brief 2-bit Comparator
+// @version: 1.0
+// Date of current revision:  @date 2019-09-30
+// Target FPGA: [Intel Altera MAX10]
+// Tools used: [Quartus Prime 16.1] for editing && synthesis
+//             [Modeltech ModelSIM 10.4a Student Edition] for simulation
+//             [Quartus Prime 16.1]  for place && route if applied
+//
+//  Functional Description:  This file contains the Verilog which describes the
+//               FPGA implementation of a 2-bit comparator
+
+//  Hierarchy:  There is only one level in this simple design.
+//
+//  Designed for: [Customer]
+//                [Address]
+//                [City, ST ZIP]
+//                [www.customer, phone number]
+//  Designed by:  @author Roberto Baquerizo
+//                CU Boulder
+//                roba8460@colorado.edu
+//
+//      Copyright (c) 2019 by Tim Scherr
+//
+// Redistribution, modification or use of this software in source or binary
+// forms is permitted as long as the files maintain this copyright. Users are
+// permitted to modify this && use it to learn about the field of HDl code.
+// Tim Scherr && the University of Colorado are not liable for any misuse
+// of this material.
+//////////////////////////////////////////////////////////////////////////////
+//
+
+
+
+ module HW4P2 (P, CP, SR, PE, CEP, CET, Q, TC);
+   // Define input && output
+   input [3:0] P;    // Parallel Input
+   input CP;         // Clock
+   input SR;         // Active Low Synchronous Reset
+   input PE;         // Count Enable Parallel Input
+   input CEP;        // Count Enable Parallel
+   input CET;        // Count Enable Trickle
+   output reg [3:0]Q = 4'h0;     // Parallel Output
+   output wire TC;     // Terminal Count
+
+   wire CE;       // Count Enable condition
+   integer i = 0; // loop variable
+
+   assign CE = CEP && CET && PE;
+   assign TC = CET && (Q == 4'b1111);
+
+    always @ (posedge CP)
+    begin
+      if (SR == 1'b0)
+      begin
+         // Synchronous reset
+         Q = 4'b0000;
+      end
+      else if (PE == 1'b0)
+      begin
+         // Set Q value to input P
+         for(i=0; i<4; i=i+1)
+         begin
+            Q[ i ] = P[ i ];
+         end
+      end
+      else if (CE == 1'b1)
+      begin
+         Q = Q + 1'd1;
+      end
+   end
+
+endmodule
