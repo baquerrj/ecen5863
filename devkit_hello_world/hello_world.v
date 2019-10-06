470a6f6e15a6f7b2e56a6fd932c3a7e11cfe38aa
Roberto Baquerizo <roba8460@colorado.edu>
2019-10-06 17:36:35 -0600

    added nios2 instance to hello_world module

--- devkit_hello_world/hello_world.v
+++ devkit_hello_world/hello_world.v
@@ -32,4 +32,12 @@ module hello_world(
 	input		[1:0] SW,
 	output	[1:0] LEDR);
 
+   assign LEDR[0] = ~SW[0];
+
+	nios_setup_v2 u0 (
+		.clk_clk                           (MAX10_CLK1_50),   // clk.clk
+		.reset_reset_n                     (1'b1)),           // reset.reset_n
+		.switch_external_connection_export (SW[1]),           // switch_external_connection.export
+		.led_external_connection_export    (LEDR[1])          // led_external_connection.export
+	);
 endmodule
